c6cc861b3672ab80039162dd1349fa314ad96207
Kor1 <prkladprkldn@gmail.com>
2023-06-30 19:45:41 +0200

    Change requirements

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-# from flask import Flask, render_template, redirect, url_for, flash
-# from flask_bootstrap import Bootstrap
-# from flask_ckeditor import CKEditor
-# from datetime import date
-# from werkzeug.security import generate_password_hash, check_password_hash
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy.orm import relationship
-# from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-# from forms import CreatePostForm
-# from flask_gravatar import Gravatar
-
 from flask import Flask, render_template
 import os
 import smtplib
