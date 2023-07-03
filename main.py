32dc1df3740a1b82e3753db48d0f8f07e2e28e3a
Kor1 <prkladprkldn@gmail.com>
2023-07-03 02:40:38 +0200

    change env

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template, request
 import os
 import smtplib
 
-OWN_EMAIL = os.environ.get("EMAIL")
-OWN_PASSWORD = os.environ.get("PASSWORD")
+# OWN_EMAIL = os.environ.get("EMAIL")
+# OWN_PASSWORD = os.environ.get("PASSWORD")
+OWN_EMAIL = os.getenv("EMAIL")
+OWN_PASSWORD = os.getenv("PASSWORD")
+
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
